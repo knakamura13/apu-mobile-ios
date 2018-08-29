6a914603335ea03729dbe1b7fa66f1c38cd7a3e5
Kyle Nakamura <kjnakamura@Kyles-MacBook-Pro.local>
2018-08-29 15:12:53 -0700

    Remove javascript for auto-login and hardcoded username and password. Open den.apu.edu (CAS) links in the second view controller.

--- APU Mobile (iOS 10)/MainVC.swift
+++ APU Mobile (iOS 10)/MainVC.swift
@@ -8,21 +8,11 @@
 
 import UIKit
 
-var shouldRepeat: Bool = true
-var testBool: Bool = false
-var testVar: Int = 0
-
 class MainVC: UIViewController, UIWebViewDelegate {
     
     @IBOutlet weak var webView: UIWebView!
     
     let environment = "mobile" // Options: mobile, mobile-beta, mobile-kyle, mobile-monte, mobile-test, mobile-upgrade, mobile-vanilla
-    let username = "knakamura13"
-    let password = "bWilkinsp7" // pls don't hack me
-    
-    var timeBool: Bool! = false
-    var timer: Timer!
-    var timer2: Timer!
     
     // Runs each time this VC appears
     override func viewWillAppear(_ animated: Bool) {
@@ -42,59 +32,20 @@ class MainVC: UIViewController, UIWebViewDelegate {
         let url = URL(string: "https://\(environment).apu.edu")
         webView.loadRequest(URLRequest(url: url!))
     }
-    
-    func loginUser(username: String?, password: String?) {
-        // Run JavaScript script to automatically login the user
-        _ = webView.stringByEvaluatingJavaScript(from:
-            "var script = document.createElement('script');" +
-            "script.type = 'text/javascript';" +
-            "script.text = \"function insertLoginDetails() { " +
-                // Force click() sign in button
-                "try { document.getElementsByTagName('a')[0].click(); }" +
-                "catch (err) { /* Do nothing if error thrown */ }" +
-            
-                // Get username and password fields by TagName
-                "var userNameField = document.getElementsByTagName('input')[1];" +
-                "var passwordField = document.getElementsByTagName('input')[2];" +
-            
-                // Inject username and password strings into input fields
-                "userNameField.value = '\(username!)';" +
-                "passwordField.value = '\(password!)';" +
-            
-                // Force click() login button
-                "var loginButton = document.getElementsByTagName('button')[1];" +
-                "loginButton.click();" +
-            "}\";" +
-            "document.getElementsByTagName('head')[0].appendChild(script);")!
-        webView.stringByEvaluatingJavaScript(from: "insertLoginDetails();")!
-    }
-    
+
     // Web page finishes loading
     func webViewDidFinishLoad(_ webView: UIWebView) {
-        if testVar < 2 {
-//            loginUser(username: self.username, password: self.password)
-        }
-        
-        var when = DispatchTime.now() + 10
-        DispatchQueue.main.asyncAfter(deadline: when) {
-            if testVar < 2 {
-//                self.loginUser(username: self.username, password: self.password)
-            }
-        }
     }
     
     // Function is called whenever a webpage is about to load.
     // Ensures that non-mobile URLs get directed to a seperate VC to prevent pages from overriding/hiding the mobile.apu.edu interface.
     func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
         let requestString = String(describing: request)
-        if requestString.contains("dashboard") {
-            testVar += 1
-        }
         clickedHyperlinkURL = requestString
         
         if navigationType == .linkClicked {
-            if requestString.contains(environment) {
-            // navigate normally
+            if requestString.contains(environment) && !(requestString.contains("den.apu.edu")) {
+                // navigate normally for native HighPoint links that are NOT den.apu.edu (CAS) links
             } else if requestString.contains("youtube.com") {
                 if let url = URL(string: String(describing: request)) {
                     UIApplication.shared.open(url, options: [:])            // open the url in Youtube, or Safari if Youtube fails
